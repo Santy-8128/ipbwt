e74f57b8ad2a6b23e058b0599f1f2f63707bddc1
Sayantan Das <sayantan@umich.edu>
2018-07-06 19:00:20 -0700

    test

---
+++ cmake-build-debug/CMakeFiles/pbwt.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pbwt.dir/src/array.c.o"
+  "CMakeFiles/pbwt.dir/src/dict.c.o"
+  "CMakeFiles/pbwt.dir/src/hash.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtCore.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtGeneticMap.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtHtslib.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtImpute.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtIO.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtLikelihood.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtMain.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtMatch.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtMerge.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtPaint.c.o"
+  "CMakeFiles/pbwt.dir/src/pbwtSample.c.o"
+  "CMakeFiles/pbwt.dir/src/utils.c.o"
+  "pbwt.pdb"
+  "pbwt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pbwt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
